ba1e96206fbc532164c90a6be821c4c916e8d019
Dana Dorn <dorndana8899@gmail.com>
2025-06-13 09:58:46 +0700

    add: src/Account for calculation and main to display

--- src/Account.java
+++ src/Account.java
@@ -1,17 +1,77 @@
-public class Account {
-    int accNumber;
-    String accHolder;
-    double accBalance;
+// BankSystem.java
+abstract class Account {
+    private String accNumber;
+    private String accHolder;
+    private double balance;
+
+    public Account(String accNumber, String accHolder, double balance) {
+        this.accNumber = accNumber;
+        this.accHolder = accHolder;
+        this.balance = balance;
+    }
+
+    public void deposit(double amount) {
+        if (amount > 0) balance += amount;
+    }
+
+    public void withdraw(double amount) {
+        if (amount > 0 && amount <= balance) balance -= amount;
+        else System.out.println("Insufficient balance or invalid amount");
+    }
+
+    public void showBalance() {
+        System.out.println("Account Number: " + accNumber);
+        System.out.println("Account Holder: " + accHolder);
+        System.out.println("Current Balance: $" + balance);
+    }
+
+    // Getters and setters
+    public String getAccNumber() { return accNumber; }
+    public String getAccHolder() { return accHolder; }
+    public double getBalance() { return balance; }
+    public void setBalance(double balance) { this.balance = balance; }
 }
 
-class SavingsAccount extends Account{
-    int deposit;
-    int withdraw;
-    int interest;
-    int balance;
+// Subclass: SavingAccount
+class SavingAccount extends Account {
+    private double interestRate;
 
+    public SavingAccount(String accNumber, String accHolder, double balance, double interestRate) {
+        super(accNumber, accHolder, balance);
+        this.interestRate = interestRate;
+    }
+
+    public double getInterestRate() { return interestRate; }
+    public void setInterestRate(double interestRate) { this.interestRate = interestRate; }
+
+    public void showSavingDetails() {
+        super.showBalance();
+        System.out.println("Interest Rate: " + interestRate + "%");
+    }
 }
 
-class CreditCard extends Account{
-    int limitAmount;
+// Subclass: CreditCardAccount
+class CreditCardAccount extends Account {
+    private double limitAmount;
+    private String expDate;
+    private boolean validation;
+
+    public CreditCardAccount(String accNumber, String accHolder, double balance,
+                             double limitAmount, String expDate, boolean validation) {
+        super(accNumber, accHolder, balance);
+        this.limitAmount = limitAmount;
+        this.expDate = expDate;
+        this.validation = validation;
+    }
+
+    public double getLimitAmount() { return limitAmount; }
+    public String getExpDate() { return expDate; }
+    public boolean isValidation() { return validation; }
+
+    public void showCreditCardDetails() {
+        super.showBalance();
+        System.out.println("Limit Amount: $" + limitAmount);
+        System.out.println("Expiry Date: " + expDate);
+        System.out.println("Validation: " + (validation ? "Valid" : "Invalid"));
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,16 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+// Main.java
 public class Main {
     public static void main(String[] args) {
+        SavingAccount sa = new SavingAccount("S123", "Alice", 1000.0, 2.5);
+        sa.deposit(500);
+        sa.withdraw(200);
+        sa.showSavingDetails();
 
+        System.out.println();
+
+        CreditCardAccount ca = new CreditCardAccount("C456", "Bob", 300.0, 1000.0, "12/25", true);
+        ca.deposit(150);
+        ca.withdraw(100);
+        ca.showCreditCardDetails();
     }
 }
